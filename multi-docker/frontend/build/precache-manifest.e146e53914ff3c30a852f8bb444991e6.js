f7e0df6bcaa1b3ca107d399a04c3e76350bef132
Arpine_Meliksetyan <a.meliqsetyan@inbox.ru>
2022-05-10 11:49:05 +0400

    Docker Course

---
+++ multi-docker/frontend/build/precache-manifest.e146e53914ff3c30a852f8bb444991e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf3c50f0169e611626adba36a54dc2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e011550ae46983b78c6",
+    "url": "/static/css/main.cd11c615.chunk.css"
+  },
+  {
+    "revision": "5b39247833b74638cad5",
+    "url": "/static/js/2.05345a47.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.05345a47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e011550ae46983b78c6",
+    "url": "/static/js/main.5700a624.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
